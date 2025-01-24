7a292815788f444043c9c6af0a6c2a78027268ab
AutomatedProcesses <33227386+AutomatedProcesses@users.noreply.github.com>
2025-01-24 23:38:20 +0000

    more models

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,18 @@
 import os
 from re import S
 from flask import Flask, request, jsonify, make_response
-from flask_marshmellow import Marshmellow
-from flask_alchemy import SQLAlchemy
+from sqlalchemy.orm import CascadeOptions
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import uuid
-import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 ma = Marshmellow(app)
+
 
 class User(db.Model):
   '''
@@ -18,3 +20,39 @@ class User(db.Model):
   '''
   __tablename__ = 'user'
   obj_id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(50), unique=True, nullable=False)
+  password = db.Column(db.String(50), nullable=False)
+  email = db.Column(db.String(50), unique=True, nullable=False)
+  date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+
+class Response(db.Model):
+  '''
+  model of response - used for storing responses
+  '''
+  __tablename__ = 'response'
+  obj_id = db.Column(db.Integer, primary_key=True)
+  date_responded = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+  
+  responder_answers = db.relationship('ResponderAnswer', cascade='all,delete', back_polulates='response')
+  
+
+class Survey(db.Model):
+  '''
+  Model of the survey with general info, hash_key and array of corresponding question
+  '''
+  __tablename__ = 'survey'
+  obj_id = db.Column(db.Integer, primary_key=True)
+  hash_key = db.Column(db.Integer, unique=True, nullable=False)
+  name = db.Column(db.String(100), unique=True, nullable=False)
+  description = db.Column(db.Text, nullable=True)
+  date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+  
+  questions = db.relationship('Question', cascade='all,delete', back_polulates='survey')
+
+
+class Question(db.Model):
+  __tablename__ = 'question'
+  obj_id = db.Column(db.Integer, primary_key=True)
+  survey_id = db.Column(db.Integer, db.ForeignKey(Survey.obj_id))
+  
