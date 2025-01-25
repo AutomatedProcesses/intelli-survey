0e0039de7943b744bafe38bbd83a585d6c593be0
AutomatedProcesses <33227386+AutomatedProcesses@users.noreply.github.com>
2025-01-25 16:10:55 +0000

    models finished

--- backend/models.py
+++ backend/models.py
@@ -35,9 +35,10 @@ class User(db.Model):
   __tablename__ = 'user'
   obj_id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(50), unique=True, nullable=False)
-  password = db.Column(db.String(50), nullable=False)
+  password = db.Column(db.String(50), nullable=False)        # consider bcrypt for encryption
   email = db.Column(db.String(50), unique=True, nullable=False)
   date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+  #is_admin = db.Column(db.Boolean, default=False)
 
 
 class Response(db.Model):
@@ -48,7 +49,7 @@ class Response(db.Model):
   obj_id = db.Column(db.Integer, primary_key=True)
   date_responded = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
   
-  responder_answers = db.relationship('ResponderAnswer', cascade='all,delete', back_polulates='response')
+  responder_answers = db.relationship('ResponderAnswer', cascade='all,delete', back_populates='response')
   
 
 class Survey(db.Model):
@@ -57,12 +58,12 @@ class Survey(db.Model):
   '''
   __tablename__ = 'survey'
   obj_id = db.Column(db.Integer, primary_key=True)
-  hash_key = db.Column(db.Integer, unique=True, nullable=False)
+  hash_key = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
   name = db.Column(db.String(100), unique=True, nullable=False)
   description = db.Column(db.Text, nullable=True)
   date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
   
-  questions = db.relationship('Question', cascade='all,delete', back_polulates='survey')
+  questions = db.relationship('Question', cascade='all,delete', back_populates='survey')
 
 
 class Question(db.Model):
@@ -72,7 +73,7 @@ class Question(db.Model):
   text = db.Column(db.Text, nullable=False)
   
   survey = db.relationship('Survey', back_polulates='questions')
-  responder_answers = db.relationship('ResponderAnswer', cascade='all,delete', back_polulates='question')
+  responder_answers = db.relationship('ResponderAnswer', cascade='all,delete', back_populates='question')
 
 
 class ResponderAnswer(db.Model):
@@ -83,6 +84,27 @@ class ResponderAnswer(db.Model):
   __tablename__ = 'responder_answer'
   response_id = db.Column(db.Integer, db.ForeignKey(Response.obj_id), primary_key=True, autoincrement=False)
   question_id = db.Column(db.Integer, db.ForeignKey(Question.obj_id), primary_key=True, autoincrement=False)
+  answer = db.Column(db.Text)
   
-  response = db.relationship('Response', back_polulates='responder_answers')
-  question = db.relationship('Question', back_polulates='responder_answers')
+  response = db.relationship('Response', back_populates='responder_answers')
+  question = db.relationship('Question', back_populates='responder_answers')
+
+
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('obj_id', 'username', 'email', 'is_admin', 'date_created')
+
+
+class QuestionSchema(ma.Schema):
+  class Meta:
+    fields = ('obj_id', 'survey_id', 'text')
+
+
+class SurveySchema(ma.Schema):
+  class Meta:
+    fields = ('obj_id', 'hash_key', 'name', 'description', 'date_created')
+
+
+class ResponderAnswerSchema(ma.Schema):
+  class Meta:
+    fields = ('response_id', 'survey_id', 'question_id', 'answer')          #survey_id
